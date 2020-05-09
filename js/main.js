9f093f3d6fd9c4753cf872a3c6379c4bda240807
Denis Neverov <denis.neverov@gmail.com>
2020-05-10 02:36:30 +0300

    Add reading from JSON

--- js/main.js
+++ js/main.js
@@ -20,7 +20,27 @@ function renderHTML(data){
   var htmlString = '';
 
   for (i = 0; i < data.length; i++){
-    htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>"
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+
+    for (ii = 0; ii < data[i].foods.likes.length; ii++){
+      if (ii == 0){
+        htmlString += data[i].foods.likes[ii];
+      } else {
+        htmlString += " and " + data[i].foods.likes[ii];
+      }
+    }
+
+    htmlString += ' and dislikes ';
+
+    for (ii = 0; ii < data[i].foods.dislikes.length; ii++){
+      if (ii == 0){
+        htmlString += data[i].foods.dislikes[ii];
+      } else {
+        htmlString += " and " + data[i].foods.dislikes[ii];
+      }
+    }
+
+    htmlString += '.</p>'
   }
 
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
